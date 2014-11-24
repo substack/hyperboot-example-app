889c2d005f73c36b64a9cccb3d260aee4ba86b99
James Halliday <mail@substack.net>
2014-11-24 09:53:29 +0100

    configure hyperboot from inside the app

--- app/main.js
+++ app/main.js
@@ -1,8 +1,15 @@
 var gamma = require('gamma');
 var observable = require('observable');
+var hboot = require('hyperboot/rpc');
 
 var result = document.querySelector('#result');
 var obs = observable.input(document.querySelector('#n'));
 obs(function (value) {
     result.textContent = gamma(Number(value));
+});
+
+var button = document.querySelector('button#config');
+button.addEventListener('click', function (ev) {
+    ev.preventDefault();
+    hboot.toggle();
 });
